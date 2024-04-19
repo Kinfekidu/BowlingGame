6a78292f24b99df66f08c3ca2b397e4e6b3bb400
Kinfe <110576373+Kinfekidu@users.noreply.github.com>
2024-04-19 20:07:27 +1200

    Programme.py
    Programme.py was added to the repository with code.

---
+++ Programme.py
@@ -0,0 +1,25 @@
+def add(self, a, b):
+        """
+        Add two numbers.
+
+        Args:
+            a (float): The first number.
+            b (float): The second number.
+
+        Returns:
+            float: The sum of a and b.
+        """
+        return a + b
+
+    def subtract(self, a, b):
+        """
+        Subtract two numbers.
+
+        Args:
+            a (float): The first number.
+            b (float): The second number.
+
+        Returns:
+            float: The result of subtracting b from a.
+        """
+        return a - b
